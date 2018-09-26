146972650163f35849c961b9908ed03b6254f3cd
ndminh1307 <ndminh1307@gmail.com>
2018-09-26 10:29:31 +0700

    Immigrants to Canada 1980-2013

---
+++ Visualization/dataset.py
@@ -0,0 +1,3 @@
+import pandas as pd
+import numpy as np
+df_can = pd.read_excel('https://ibm.box.com/shared/static/lw190pt9zpy5bd1ptyg2aw15awomz9pu.xlsx', sheet_name='Canada by Citizenship', skiprows=range(20),skipfooter=2)
